98e35915ed7c68b895a03bedf18b21522f97b655
Angela Londoño <l.angela05@gmail.com>
2022-07-29 21:30:41 -0500

    Se implementan los ataques del jugador y enemigo

--- js/mokepon.js
+++ js/mokepon.js
@@ -1,27 +1,96 @@
-window.addEventListener('load', iniciarJuego);
+let ataqueJugador
+let ataqueEnemigo
+
+window.addEventListener('load', iniciarJuego)
+
 
 /* Carga los elementos del HTML */
 function iniciarJuego(){
 //Declaración de variables definidas en el HTML
-let btnMascota = document.getElementById('boton-mascota');
-btnMascota.addEventListener('click', seleccionarMascotaJugador);
-
+let btnMascota = document.getElementById('boton-mascota')
+btnMascota.addEventListener('click', seleccionarMascotaJugador)
+let botonFuego=document.getElementById('boton-fuego')
+botonFuego.addEventListener('click',ataqueFuego)
+let botonAgua=document.getElementById('boton-agua')
+botonAgua.addEventListener('click',ataqueAgua)
+let botonTierra=document.getElementById('boton-tierra')
+botonTierra.addEventListener('click',ataqueTierra)
 }
 
 /* Esta funcion permite saber que mascota eligió el jugador */
 function seleccionarMascotaJugador(){
-
-    let inputHipodoge = document.getElementById('hipodoge');
-    let inputCapepo = document.getElementById('capipepo');
-    let inputRatigueya = document.getElementById('ratigueya');
+    let inputHipodoge = document.getElementById('hipodoge')
+    let inputCapepo = document.getElementById('capipepo')
+    let inputRatigueya = document.getElementById('ratigueya')
+    let spanMascotaJugador = document.getElementById('mascota-jugador')
 
     if(inputHipodoge.checked){
-        alert("Seleccionaste a Hipodoge");
+        spanMascotaJugador.innerHTML= 'Hipodoge'
     }else if(inputCapepo.checked){
-        alert("Seleccionaste a Capipepo");
+        spanMascotaJugador.innerHTML= 'Capipepo'
     }else if(inputRatigueya.checked){
-        alert("Seleccionaste a Ratigueya");
+        spanMascotaJugador.innerHTML= 'Ratigueya'
     }else {
-        alert("Selecciona una mascota");
+        alert("Selecciona una mascota")
     }
-}   
+    seleccionarMascotaEnemigo()
+} 
+
+/* Esta funcion permite saber que mascota eligió el enemigo */
+function seleccionarMascotaEnemigo(){
+    let mascotaAleatorio = aleatorio(1,3)
+    let spanMascotaEnemigo = document.getElementById('mascota-enemigo')
+
+    if(mascotaAleatorio==1){
+        spanMascotaEnemigo.innerHTML= 'Hipodoge'
+    }else if(mascotaAleatorio==2){
+        spanMascotaEnemigo.innerHTML= 'Capipepo'
+    }else {
+        spanMascotaEnemigo.innerHTML= 'Ratigueya'
+    }
+}
+
+//Funciones de ataque del jugador
+function ataqueFuego() {
+    ataqueJugador = 'FUEGO'
+    ataqueAleatorioEnemigo()
+}
+function ataqueAgua() {
+    ataqueJugador = 'AGUA'
+    ataqueAleatorioEnemigo()
+}
+function ataqueTierra() {
+    ataqueJugador = 'TIERRA'
+    ataqueAleatorioEnemigo()
+}
+
+/* Función para el ataque aleatorio del enemigo */
+function ataqueAleatorioEnemigo(){
+    let ataqueAleatorio = aleatorio(1,3)
+    
+    if(ataqueAleatorio==1){
+        ataqueEnemigo= 'FUEGO'
+    }else if(ataqueAleatorio==2){
+        ataqueEnemigo= 'AGUA'
+    }else {
+        ataqueEnemigo= 'TIERRA'
+    }    
+
+    crearMensaje()
+} 
+
+/* Funcón que permite enviar los mesajes del estado del juego al HTML */
+function crearMensaje(){
+    let sectioMensaje = document.getElementById('mensajes')
+    let parrafo = document.createElement('p')
+    
+    parrafo.innerHTML = 'Tu mascota ataco con ' + ataqueJugador + ' y la mascota del enemigo atacó con ' + ataqueEnemigo
+    sectioMensaje.appendChild(parrafo)
+}
+
+
+/* Crear mascota aleatoria para el enemigo */
+function aleatorio(min,max){
+    return Math.floor(Math.random()* (max - min + 1)+min)
+}
+
